2fda6ff77658f1899a289e1e51ff3eb8f7b62d9c
SQverse <241220060@unmuhpnk.ac.id>
2026-07-24 20:28:01 +0700

    test continous deployment

---
+++ github-webhook.php
@@ -0,0 +1,8 @@
+<?php
+// Ganti dengan Repository Path kamu yang ada di cPanel
+$repo_dir = '/home/vbax9396/repositories/vbat-ponsel-duplicate';
+
+// Menjalankan git pull secara otomatis
+$output = shell_exec("cd {$repo_dir} && git pull origin main 2>&1");
+echo "<pre>$output</pre>";
+?>
